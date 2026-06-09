ab8ca067209071277438c983eeaa51de1682540e
Minh <jinneverett@gmail.com>
2026-06-09 11:17:27 +0700

    Hello world

---
+++ notification-service/src/test/java/quant/notificationservice/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package quant.notificationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ payment-service/src/test/java/quant/paymentservice/PaymentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package quant.paymentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
