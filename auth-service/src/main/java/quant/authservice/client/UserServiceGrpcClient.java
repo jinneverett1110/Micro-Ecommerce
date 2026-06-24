1f9054ab00ce7e9418618e4224b6e063d4cf0bfd
Minh <jinneverett@gmail.com>
2026-06-24 15:21:11 +0700

    hotfix: compress grpc-proto to test

---
+++ auth-service/src/main/java/quant/authservice/client/UserServiceGrpcClient.java
@@ -0,0 +1,19 @@
+package quant.authservice.client;
+
+import net.devh.boot.grpc.client.inject.GrpcClient;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserServiceGrpcClient {
+
+    @GrpcClient("user-service")
+    private UserServiceGrpc.UserServiceBlockingStub userServiceStub;
+
+    public void createUserProfile(String id, String email) {
+        CreateUserRequest request = CreateUserRequest.newBuilder()
+                .setUserId(id)
+                .setEmail(email)
+                .build();
+        userServiceStub.createUser(request);
+    }
+}
