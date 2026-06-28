a79270aba67b2373ea680ab3609aeb94bf6643a3
Minh <jinneverett@gmail.com>
2026-06-28 16:06:16 +0700

    feat: complete features

--- notification-service/src/main/java/quant/notificationservice/dto/NotificationMessage.java
+++ notification-service/src/main/java/quant/notificationservice/dto/NotificationMessage.java
@@ -1,4 +1,19 @@
+// NotificationMessage.java
 package quant.notificationservice.dto;
 
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.Instant;
+
+@Data
+@Builder
 public class NotificationMessage {
+    private String id;
+    private String userId;
+    private String type;
+    private String title;
+    private String message;
+    private String orderId;
+    private Instant createdAt;
 }
